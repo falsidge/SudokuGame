139f4f466c789600f24f5d3bad4f394cfc327f62
Karson Tran <karsont@uci.edu>
2023-08-27 10:56:10 -0700

    create row print

--- sudoku.cpp
+++ sudoku.cpp
@@ -17,7 +17,11 @@ SudokuBoard::~SudokuBoard() {
 void SudokuBoard::print(std::ostream &out) const {
     for (int i = 1; i <= size; ++i) {
         for(int j = 1; j <= size; ++j) {
-            
+            out << gameBoard[i][j];
+
+            if (j % (size / 3) == 0) {
+                out << '|';
+            }
         }
     }
 }
