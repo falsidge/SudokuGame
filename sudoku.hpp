10471e25e79adf0e7fb446df6fbdc001eb4709ba
Karson Tran <karsont@uci.edu>
2023-08-27 10:31:33 -0700

    create constructor/destructor

--- sudoku.hpp
+++ sudoku.hpp
@@ -1,10 +1,25 @@
 #ifndef SUDOKU_HPP
 #define SUDOKU_HPP
 
+constexpr int BOARDSIZE = 9;
+
 class SudokuBoard {
 public:
+    SudokuBoard();
+
+    SudokuBoard(const SudokuBoard &s);
+
+    SudokuBoard &operator=(const SudokuBoard &s);
+
+
+    SudokuBoard(SudokuBoard &&s);
+
+    SudokuBoard &operator=(SudokuBoard &&s);
+
+    ~SudokuBoard();
 
 private:
-}
+    int gameBoard[BOARDSIZE];
+};
 
 #endif
