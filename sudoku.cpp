bd80b4a6db95189ea1a15a303d7e4a36ada56116
Karson Tran <karsont@uci.edu>
2023-08-27 10:43:44 -0700

    assign values to array

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,1 +1,7 @@
 #include "sudoku.hpp"
+
+SudokuBoard::SudokuBoard() {
+    for (int i = 0; i < size; ++i) {
+        gameBoard[i] = new int[size]{0};
+    }
+}
--- sudoku.hpp
+++ sudoku.hpp
@@ -24,7 +24,7 @@ public:
 
 private:
     int size = BOARDSIZE;
-    int gameBoard[BOARDSIZE];
+    int* gameBoard[BOARDSIZE];
 };
 
 #endif
