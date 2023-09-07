ff1c41d0fadb849ea06a40ce52ef851d776466d4
Karson Tran <karsont@uci.edu>
2023-09-06 17:17:43 -0700

    fix test

--- gtests/sudoku_gtests.cpp
+++ gtests/sudoku_gtests.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "../source/sudoku.h"
 
-TEST(SudokuLogic, CreatingBoard) {
+TEST(SudokuBoardSetUp, CreatingBoard) {
     SudokuBoard game = SudokuBoard();
 
     auto board = game.getGameBoard();
@@ -23,7 +23,7 @@ TEST(SudokuLogic, CreatingBoard) {
     }
 }
 
-TEST(SudokuLogic, AnchoredNumberPlacement) {
+TEST(SudokuBoardSetUp, AnchoredNumberPlacement) {
     SudokuBoard game = SudokuBoard();
 
     game.insertAnchoredNumber(4, 6, 7);
@@ -59,7 +59,7 @@ TEST(SudokuLogic, AnchoredNumberPlacement) {
     EXPECT_EQ(anchoredCoorSet.find((4 - 1) * 9 + (8 - 1)), anchoredCoorSet.end());
 }
 
-TEST(SudokuLogic, PlayerNumberPlacement) {
+TEST(SudokuBoardSetUp, PlayerNumberPlacement) {
     SudokuBoard game = SudokuBoard();
 
     game.playerInsertNumber(6, 8, 4);
@@ -86,3 +86,36 @@ TEST(SudokuLogic, PlayerNumberPlacement) {
         }
     }
 }
+
+
+TEST(SudokuBoardSetUp, PlayerAnchoredInteractionPlacement) {
+    SudokuBoard game = SudokuBoard();
+
+    game.insertAnchoredNumber(1, 6, 1);
+    game.insertAnchoredNumber(2, 3, 5);
+    game.insertAnchoredNumber(3, 3, 8);
+    game.insertAnchoredNumber(4, 3, 6);
+    game.insertAnchoredNumber(5, 8, 9);
+    game.insertAnchoredNumber(6, 2, 1);
+    game.insertAnchoredNumber(7, 1, 4);
+    game.insertAnchoredNumber(8, 7, 1);
+    game.insertAnchoredNumber(9, 5, 4);
+
+    int expectedBoard[9][9] = {{0,0,0, 7,0,0, 0,0,0},
+                               {6,0,0, 0,0,0, 0,0,0},
+                               {0,0,0, 0,2,4, 0,3,0},
+                               {0,0,0, 0,0,0, 0,0,0},
+                               {0,0,0, 9,0,0, 0,0,0},
+                               {1,0,0, 0,0,0, 0,0,0},
+                               {8,0,0, 0,0,0, 0,0,0},
+                               {0,0,0, 0,0,0, 0,0,5},
+                               {0,0,0, 0,0,0, 0,0,0}};
+
+    auto board = game.getGameBoard();
+
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            EXPECT_EQ(board[i][j], expectedBoard[i][j]);
+        }
+    }
+} 
