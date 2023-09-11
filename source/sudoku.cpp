0b953668f6934575ccf21ff163fcd02414886f63
Karson Tran <karsont@uci.edu>
2023-09-11 10:20:19 -0700

    delete parameter

--- source/sudoku.cpp
+++ source/sudoku.cpp
@@ -210,7 +210,7 @@ int SudokuBoard::calGridNumber(int row, int col) const {
     return (row - 1) * (size) + col - 1;
 }
 
-void SudokuBoard::generateNewBoard(int numberOfValues) {
+void SudokuBoard::generateNewBoard() {
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
             gameBoard[i][j] = 0;
