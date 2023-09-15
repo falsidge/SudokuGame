532c04f6a678ee434c5a27c2187368dbd81e1015
Karson Tran <karsont@uci.edu>
2023-09-14 19:35:21 -0700

    add all tests for wrong grid function

--- gtests/sudoku_gtests.cpp
+++ gtests/sudoku_gtests.cpp
@@ -866,6 +866,54 @@ TEST(SudokuBoardWrongBoardGrids, WrongGrids3) {
     EXPECT_EQ(wrongGrids.size(), predictedWrongGrids.size());
 }
 
+TEST(SudokuBoardWrongBoardGrids, WrongGrids4) {
+    int row1[] = {4,3,5, 2,6,9, 7,8,1};
+    int row2[] = {6,8,2, 5,7,1, 4,9,3};
+    int row3[] = {1,9,7, 8,3,4, 5,6,2};
+    int row4[] = {8,2,6, 1,9,5, 3,4,7};
+    int row5[] = {3,7,4, 6,8,2, 9,1,5};
+    int row6[] = {9,5,1, 7,4,3, 6,2,8};
+    int row7[] = {5,1,9, 3,2,6, 8,7,4};
+    int row8[] = {2,4,8, 9,5,7, 1,3,6};
+    int row9[] = {7,6,3, 4,1,8, 2,5,0};
+
+    int* testBoard[] = {row1, row2, row3, row4, row5, row6, row7, row8, row9};
+
+    SudokuBoard game = SudokuBoard(testBoard);
+
+    std::set<int> wrongGrids = game.getAllWrongGrids();
+    std::set<int> predictedWrongGrids {80};
+
+    for (auto i = wrongGrids.begin(); i != wrongGrids.end(); ++i) {
+        EXPECT_NE(predictedWrongGrids.find(*i), predictedWrongGrids.end());
+    }
+    EXPECT_EQ(wrongGrids.size(), predictedWrongGrids.size());
+}
+
+TEST(SudokuBoardWrongBoardGrids, WrongGrids5) {
+    int row1[] = {4,3,5, 2,6,9, 7,8,1};
+    int row2[] = {6,8,2, 5,7,1, 4,9,3};
+    int row3[] = {1,9,7, 8,3,4, 5,6,2};
+    int row4[] = {8,2,6, 1,9,5, 3,4,7};
+    int row5[] = {3,7,4, 6,0,2, 9,1,5};
+    int row6[] = {9,5,1, 7,4,3, 6,2,8};
+    int row7[] = {5,1,9, 3,2,6, 8,7,4};
+    int row8[] = {2,4,8, 9,5,7, 1,3,6};
+    int row9[] = {7,6,3, 4,1,8, 2,5,9};
+
+    int* testBoard[] = {row1, row2, row3, row4, row5, row6, row7, row8, row9};
+
+    SudokuBoard game = SudokuBoard(testBoard);
+
+    std::set<int> wrongGrids = game.getAllWrongGrids();
+    std::set<int> predictedWrongGrids {40};
+
+    for (auto i = wrongGrids.begin(); i != wrongGrids.end(); ++i) {
+        EXPECT_NE(predictedWrongGrids.find(*i), predictedWrongGrids.end());
+    }
+    EXPECT_EQ(wrongGrids.size(), predictedWrongGrids.size());
+}
+
 TEST(SudokuBoardGeneration, CreateSolvedPuzzle) {
     SudokuBoard game = SudokuBoard();
 
