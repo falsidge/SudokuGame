a6d2966b76796db0ab4501fd4e6cfe9bc8ca80c8
Karson Tran <karsont@uci.edu>
2023-09-14 19:53:45 -0700

    create user input for board creation

--- source/sudoku_main.cpp
+++ source/sudoku_main.cpp
@@ -2,20 +2,24 @@
 
 #include "sudoku.h"
 
-void test() {
-    SudokuBoard game = SudokuBoard(4);
+SudokuBoard promptGameCreation() {
+    while (true) {
+        try {
+            std::cout << "How many squares would you like one of the 3x3 grids to have? (minimum of size of 1, maximum size of 99)" << std::endl;
 
-    game.print(std::cout);
+            int gridSize;
+            std::cin >> gridSize;
 
-    game.insertAnchoredNumber(4, 6, 7);
-
-    game.insertAnchoredNumber(1, 1, 1);
-
-    game.insertAnchoredNumber(10, 8, 1);
-
-    game.print(std::cout);
+            return SudokuBoard(gridSize);
+        } catch (const std::invalid_argument& e){
+            std::cout << "Sorry there was an issue with your input: " << std::endl;
+            std::cout << e.what() << std::endl;
+            std::cout << "please try again" << std::endl;
+            std::cout << '\n';
+        }
+    }
 }
 
 int main() {
-    test();
+    SudokuBoard mainGame = promptGameCreation();
 }
