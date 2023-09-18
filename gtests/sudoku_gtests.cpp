492ea8f393e70b3396704141adb6288f887342b1
Karson Tran <karsont@uci.edu>
2023-09-18 14:01:12 -0700

    fix gtests

--- gtests/sudoku_gtests.cpp
+++ gtests/sudoku_gtests.cpp
@@ -987,7 +987,7 @@ TEST(SudokuBoardGeneration, CreateSolvedPuzzle3) {
 TEST(SudokuBoardGeneration, CreatePlayablePuzzle) {
     SudokuBoard game = SudokuBoard();
 
-    game.generateNewPlayableBoard(16);
+    game.generateNewPlayableBoard(0.197);
 
     EXPECT_EQ(game.getAnchoredcoor().size(), 9 * 9 - 16);
 }
@@ -995,7 +995,7 @@ TEST(SudokuBoardGeneration, CreatePlayablePuzzle) {
 TEST(SudokuBoardGeneration, CreatePlayablePuzzle2) {
     SudokuBoard game = SudokuBoard();
 
-    game.generateNewPlayableBoard(32);
+    game.generateNewPlayableBoard(0.39);
 
     EXPECT_EQ(game.getAnchoredcoor().size(), 9 * 9 - 32);
 }
